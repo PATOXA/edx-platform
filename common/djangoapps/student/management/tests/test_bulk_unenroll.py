a67c2d961c82d38c07d3ffe4fd807c002f5b944d
uzairr <uzairr@yahoo.com>
2020-03-27 19:16:11 +0500

    Refactor bulk unenroll mgmt cmd
    Bulk unenroll command is not supporting unenrolling all the learners
    from multiple courses.This PR will enhance this mgmt cmd so that
    it can support un-enrollment of learners from multiple courses.
    
    PROD-1347

--- common/djangoapps/student/management/tests/test_bulk_unenroll.py
+++ common/djangoapps/student/management/tests/test_bulk_unenroll.py
@@ -28,8 +28,6 @@ class BulkUnenrollTests(SharedModuleStoreTestCase):
 
         self.user_info = [
             ('amy', 'amy@pond.com', 'password'),
-            ('rory', 'rory@theroman.com', 'password'),
-            ('river', 'river@song.com', 'password')
         ]
         self.enrollments = []
         self.users = []
@@ -41,7 +39,7 @@ class BulkUnenrollTests(SharedModuleStoreTestCase):
 
     def _write_test_csv(self, csv, lines):
         """Write a test csv file with the lines provided"""
-        csv.write(b"user_id,username,email,course_id\n")
+        csv.write(b"username,course_id\n")
         for line in lines:
             csv.write(six.b(line))
         csv.seek(0)
@@ -50,36 +48,22 @@ class BulkUnenrollTests(SharedModuleStoreTestCase):
     def test_invalid_course_key(self):
         """Verify in case of invalid course key warning is logged."""
         with NamedTemporaryFile() as csv:
-            csv = self._write_test_csv(csv, lines=["111,amy,amy@pond.com,test_course\n"])
+            csv = self._write_test_csv(csv, lines=["amy,test_course\n"])
 
             with LogCapture(LOGGER_NAME) as log:
                 call_command("bulk_unenroll", "--csv_path={}".format(csv.name))
-                expected_message = 'Invalid course id {}, skipping un-enrollement for {}, {}'.\
-                    format('test_course', 'amy', 'amy@pond.com')
+                expected_message = 'Invalid course id {}, skipping un-enrollement.'.\
+                    format('test_course')
 
                 log.check_present(
                     (LOGGER_NAME, 'WARNING', expected_message)
                 )
 
-    def test_user_not_enrolled(self):
-        """Verify in case of user not enrolled in course warning is logged."""
-        with NamedTemporaryFile() as csv:
-            csv = self._write_test_csv(csv, lines=["111,amy,amy@pond.com,course-v1:edX+DemoX+Demo_Course\n"])
-
-            with LogCapture(LOGGER_NAME) as log:
-                call_command("bulk_unenroll", "--csv_path={}".format(csv.name))
-                expected_message = 'Enrollment for the user {} in course {} does not exist!'.\
-                    format('amy', 'course-v1:edX+DemoX+Demo_Course')
-
-                log.check_present(
-                    (LOGGER_NAME, 'INFO', expected_message)
-                )
-
     def test_bulk_un_enroll(self):
         """Verify users are unenrolled using the command."""
         lines = [
-            str(enrollment.user.id) + "," + enrollment.user.username + "," +
-            enrollment.user.email + "," + str(enrollment.course.id) + "\n"
+            enrollment.user.username + "," +
+            str(enrollment.course.id) + "\n"
             for enrollment in self.enrollments
         ]
         with NamedTemporaryFile() as csv:
@@ -105,7 +89,7 @@ class BulkUnenrollTests(SharedModuleStoreTestCase):
 
     def test_users_unenroll_successfully_logged(self):
         """Verify users unenrolled are logged """
-        lines = "user_id,username,email,course_id\n"
+        lines = "username,course_id\n"
         users_unenrolled = {}
         for enrollment in self.enrollments:
             username = enrollment.user.username
@@ -114,20 +98,19 @@ class BulkUnenrollTests(SharedModuleStoreTestCase):
             else:
                 users_unenrolled[username] = [str(enrollment.course.id).encode('utf-8')]
 
-            lines += str(enrollment.user.id) + "," + username + "," + \
-                enrollment.user.email + "," + str(enrollment.course.id) + "\n"
+            lines += username + "," + str(enrollment.course.id) + "\n"
 
         csv_file = SimpleUploadedFile(name='test.csv', content=lines.encode('utf-8'), content_type='text/csv')
         BulkUnenrollConfiguration.objects.create(enabled=True, csv_file=csv_file)
 
         with LogCapture(LOGGER_NAME) as log:
             call_command("bulk_unenroll")
+            for key, value in users_unenrolled.items():
+                username, course = key, value[0].decode('utf-8')
             log.check(
                 (
                     LOGGER_NAME,
                     'INFO',
-                    'Following users have been unenrolled successfully from the following courses:'
-                    ' {users_unenrolled}'.format(users_unenrolled=["{}:{}".format(k, v) for k, v in
-                                                                   users_unenrolled.items()])
+                    'User [{}] have been successfully unenrolled from the course: {}'.format(username, course)
+                    )
                 )
-            )
