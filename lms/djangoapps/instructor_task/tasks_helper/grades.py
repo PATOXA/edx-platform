4598232d6df123ac49af49f5c2684dd6aec4bfbd
SaadYousaf <saadyousaf@A006-00314.local>
2020-04-03 16:54:23 +0500

    add investigation logs.

--- lms/djangoapps/instructor_task/tasks_helper/grades.py
+++ lms/djangoapps/instructor_task/tasks_helper/grades.py
@@ -131,6 +131,8 @@ class GradeReportBase(object):
             This generator method fetches & loads the enrolled user objects on demand which in chunk
             size defined. This method is a workaround to avoid out-of-memory errors.
             """
+            if six.text_type(course_id) == 'course-v1:MITx+CTL.SC0x+1T2019':
+                context.update_status('ProblemGradeReport Investigation log: Starting batching of enrolled students')
             filter_kwargs = {
                 'courseenrollment__course_id': course_id,
             }
@@ -139,6 +141,8 @@ class GradeReportBase(object):
 
             user_ids_list = get_user_model().objects.filter(**filter_kwargs).values_list('id', flat=True).order_by('id')
             user_chunks = grouper(user_ids_list)
+            if six.text_type(course_id) == 'course-v1:MITx+CTL.SC0x+1T2019':
+                context.update_status('ProblemGradeReport Investigation log: user chunks created successfully')
             for user_ids in user_chunks:
                 user_ids = [user_id for user_id in user_ids if user_id is not None]
                 min_id = min(user_ids)
@@ -148,10 +152,13 @@ class GradeReportBase(object):
                     id__lte=max_id,
                     **filter_kwargs
                 ).select_related('profile')
+                if six.text_type(course_id) == 'course-v1:MITx+CTL.SC0x+1T2019':
+                    context.update_status('ProblemGradeReport Investigation log: user chunk yielded successfully')
                 yield users
 
         course_id = context.course_id
-
+        if six.text_type(course_id) == 'course-v1:MITx+CTL.SC0x+1T2019':
+            context.update_status('ProblemGradeReport Investigation log: Start of getting enrolled users')
         return get_enrolled_learners_for_course(course_id=course_id, verified_only=context.report_for_verified_only)
 
     def _compile(self, context, batched_rows):
@@ -757,6 +764,8 @@ class ProblemGradeReport(GradeReportBase):
         """
         Returns a list of rows for the given users for this report.
         """
+        if six.text_type(context.course_id) == 'course-v1:MITx+CTL.SC0x+1T2019':
+            context.update_status('ProblemGradeReport Investigation log: Start of row creation for users')
         success_rows, error_rows = [], []
         for student, course_grade, error in CourseGradeFactory().iter(users, context.course):
             student_fields = [getattr(student, field_name) for field_name in header_row]
